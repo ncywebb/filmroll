dcd3b295d932532c420425d6931c37502a59f68c
hanhanxue <hanhanxue.hhx@gmail.com>
2022-08-08 22:00:24 -0700

    init

---
+++ pages/index.js
@@ -0,0 +1,105 @@
+
+
+import fs from 'fs'
+import path from 'path'
+
+import NextImage from 'next/image'
+
+const sizeOf = require('image-size')
+
+
+import Head from 'next/head'
+
+
+
+export default function Home( {photos} ) {
+  return (
+    <>
+      <Head>
+        <title>Nancy Webb</title>
+      </Head>
+
+      <h1>Nancy Webb LOL</h1>
+
+      <main className={`section sectionDefault`}>
+      <div className={`wrapper`}> 
+      <div className={`letterbox_64_128`}>
+      
+      
+          <div className={`tilesContainer textAlignCenter`}>
+
+
+          
+              {photos.map((i, index) => (
+
+                <NextImage
+                  src={`/photos/${i.photo_filename}`}  
+                  width={i.dimensions.width}
+                  height={i.dimensions.height}
+                  key={index}
+                />
+
+              ))}
+          
+          </div>
+          
+      
+      </div>
+      </div>
+      </main>
+    </>
+
+  )
+}
+
+
+
+
+export const getStaticProps = async () => {
+
+  const root = process.cwd()
+  const filepath = path.join(root, 'public/photos')
+  const files = fs.readdirSync(filepath)
+
+  const filter_files = files.filter(i => i.match(/^\_/g) === null )
+
+
+  const photos =  filter_files.reverse().map(i => {
+
+    const root = process.cwd()
+    const filepath = path.join(root, 'public/photos', i)
+    const dimensions = sizeOf(filepath)
+
+    const MAX_WIDTH = 960
+    const MAX_HEIGHT = 760
+
+    let ratio
+
+    if (dimensions.width > MAX_WIDTH) {
+        ratio = dimensions.height / dimensions.width
+        dimensions.width = MAX_WIDTH
+        dimensions.height = dimensions.width * ratio
+    }
+    if (dimensions.height > MAX_HEIGHT) {
+        ratio = dimensions.width / dimensions.height
+        dimensions.height = MAX_HEIGHT
+        dimensions.width = dimensions.height * ratio
+    }
+
+
+    return {
+        photo_filename: i,
+        dimensions,
+    }
+
+
+
+  })
+
+
+  return {
+      props: {
+          photos,
+      }
+  }
+}
